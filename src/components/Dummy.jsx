078a246a054be50dc2cb580bb5dc27ba089ecb1f
Deep Kanani <deepkanani999@gmail.com>
2025-04-24 11:51:49 +0530

    Intial commit after some changes.

---
+++ src/components/Dummy.jsx
@@ -0,0 +1,117 @@
+<header className="header-middle bg-color-one border-bottom border-gray-100">
+  <div className="container container-lg">
+    <nav className="header-inner flex-between">
+      {/* Logo Start */}
+      {/* <div className="logo">
+              <Link href="/" className="link">
+                <img src="assets/images/logo/logo.png" alt="Logo" />
+              </Link>
+            </div> */}
+      {/* Logo End  */}
+      {/* form location Start */}
+      {/* <form
+              action='#'
+              className='flex-align flex-wrap form-location-wrapper'
+            >
+              <div className='search-category d-flex h-48 select-border-end-0 radius-end-0 search-form d-sm-flex d-none'>
+                <select
+                  defaultValue={1}
+                  className='js-example-basic-single border border-gray-200 border-end-0'
+                  name='state'
+                >
+                  <option value={1}>All Categories</option>
+                  <option value={1}>Grocery</option>
+                  <option value={1}>Breakfast &amp; Dairy</option>
+                  <option value={1}>Vegetables</option>
+                  <option value={1}>Milks and Dairies</option>
+                  <option value={1}>Pet Foods &amp; Toy</option>
+                  <option value={1}>Breads &amp; Bakery</option>
+                  <option value={1}>Fresh Seafood</option>
+                  <option value={1}>Fronzen Foods</option>
+                  <option value={1}>Noodles &amp; Rice</option>
+                  <option value={1}>Ice Cream</option>
+                </select>
+                <div className='search-form__wrapper position-relative'>
+                  <input
+                    type='text'
+                    className='search-form__input common-input py-13 ps-16 pe-18 rounded-end-pill pe-44'
+                    placeholder='Search for a product or brand'
+                  />
+                  <button
+                    type='submit'
+                    className='w-32 h-32 bg-main-600 rounded-circle flex-center text-xl text-white position-absolute top-50 translate-middle-y inset-inline-end-0 me-8'
+                  >
+                    <i className='ph ph-magnifying-glass' />
+                  </button>
+                </div>
+              </div>
+              <div className='location-box bg-white flex-align gap-8 py-6 px-16 rounded-pill border border-gray-100'>
+                <span className='text-gray-900 text-xl d-xs-flex d-none'>
+                  <i className='ph ph-map-pin' />
+                </span>
+                <div className='line-height-1'>
+                  <span className='text-gray-600 text-xs'>Your Location</span>
+                  <div className='line-height-1'>
+                    <select
+                      defaultValue={1}
+                      className='js-example-basic-single border border-gray-200 border-end-0'
+                      name='state'
+                    >
+                      <option value={1}>Alabama</option>
+                      <option value={1}>Alaska</option>
+                      <option value={1}>Arizona</option>
+                      <option value={1}>Delaware</option>
+                      <option value={1}>Florida</option>
+                      <option value={1}>Georgia</option>
+                      <option value={1}>Hawaii</option>
+                      <option value={1}>Indiana</option>
+                      <option value={1}>Marzland</option>
+                      <option value={1}>Nevada</option>
+                      <option value={1}>New Jersey</option>
+                      <option value={1}>New Mexico</option>
+                      <option value={1}>New York</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </form> */}
+      {/* form location start */}
+      {/* Header Middle Right start */}
+      {/* <div className='header-right flex-align d-lg-block d-none'>
+              <div className='flex-align flex-wrap gap-12'>
+                <button
+                  type='button'
+                  className='search-icon flex-align d-lg-none d-flex gap-4 item-hover'
+                >
+                  <span className='text-2xl text-gray-700 d-flex position-relative item-hover__text'>
+                    <i className='ph ph-magnifying-glass' />
+                  </span>
+                </button>
+                <Link href='/wishlist' className='flex-align gap-4 item-hover'>
+                  <span className='text-2xl text-gray-700 d-flex position-relative me-6 mt-6 item-hover__text'>
+                    <i className='ph ph-heart' />
+                    <span className='w-16 h-16 flex-center rounded-circle bg-main-600 text-white text-xs position-absolute top-n6 end-n4'>
+                      2
+                    </span>
+                  </span>
+                  <span className='text-md text-gray-500 item-hover__text d-none d-lg-flex'>
+                    Wishlist
+                  </span>
+                </Link>
+                <Link href='/cart' className='flex-align gap-4 item-hover'>
+                  <span className='text-2xl text-gray-700 d-flex position-relative me-6 mt-6 item-hover__text'>
+                    <i className='ph ph-shopping-cart-simple' />
+                    <span className='w-16 h-16 flex-center rounded-circle bg-main-600 text-white text-xs position-absolute top-n6 end-n4'>
+                      2
+                    </span>
+                  </span>
+                  <span className='text-md text-gray-500 item-hover__text d-none d-lg-flex'>
+                    Cart
+                  </span>
+                </Link>
+              </div>
+            </div> */}
+      {/* Header Middle Right End  */}
+    </nav>
+  </div>
+</header>;
